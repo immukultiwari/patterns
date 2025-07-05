caf20e9afbbfb0c6d659ff00808cee1d9187b64f
immukultiwari <immukultiwari@gmail.com>
2025-07-05 17:00:05 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number of rows and column you want\n";
+    cin>>n;
+    char ch= 'A';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
